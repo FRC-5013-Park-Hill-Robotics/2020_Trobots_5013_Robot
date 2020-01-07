15d6539352f7497a51c7e93a3b6e40f9c742706e
5013-Programmer <46497490+5013-Programmer@users.noreply.github.com>
2020-01-07 17:07:32 -0600

    Adding the drive motors to the robot map
    Reinart

--- Trobots2020Code-Imported/src/main/java/frc/robot/RobotMap.java
+++ Trobots2020Code-Imported/src/main/java/frc/robot/RobotMap.java
@@ -16,9 +16,10 @@ package frc.robot;
 public class RobotMap {
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
-
+  public static int leftMotor1 = 1;
+  public static int lefttMotor2 = 2;
+  public static int rightMotor1 = 3;
+  public static int rightMotor2 = 4;
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
