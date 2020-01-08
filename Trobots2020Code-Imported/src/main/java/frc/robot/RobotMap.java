d82b7a021fdebd247c9ae645a7cad51d19062d3f
5013-Programmer <46497490+5013-Programmer@users.noreply.github.com>
2020-01-08 09:11:25 -0600

    Made the RobotMap all nice and pretty
    Reinart

--- Trobots2020Code-Imported/src/main/java/frc/robot/RobotMap.java
+++ Trobots2020Code-Imported/src/main/java/frc/robot/RobotMap.java
@@ -16,15 +16,23 @@ package frc.robot;
 public class RobotMap {
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
+  
+  //============Drive Train Motors============
   public static int LEFT_MOTOR_1 = 0;
   public static int LEFT_MOTOR_2 = 1;
   public static int RIGHT_MOTOR_1 = 2;
   public static int RIGHT_MOTOR_2 = 3;
+
+  //============Controller Port============
   public static final int DRIVER_CONTROLLER = 0;
+
+  //============Controller sticks============
   public static final int RIGHT_STICK_X = 4;
   public static final int RIGHT_STICK_Y = 5;
   public static final int LEFT_STICK_X = 0;
   public static final int LEFT_STICK_Y = 1;
+
+  
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
   // public static int rangefinderPort = 1;
